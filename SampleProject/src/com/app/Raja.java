217b8684cd8b1dc507afaaa4787d74663ef34050
rajabyjava <rajasekhar.pcr@gmail.com>
2019-05-01 12:30:07 +0530

    character programs....

--- SampleProject/src/com/app/Raja.java
+++ SampleProject/src/com/app/Raja.java
@@ -1,7 +1,7 @@
 package com.app;
 
 public class Raja {
-	Raja(){
+	/*Raja(){
 		System.out.println("parent const");
 	}
 	static{ System.out.println("parent block");}
@@ -22,3 +22,56 @@ Prem(){
 		}
 	}
 
+*/
+	
+	/*public static void main(String[] args) {
+		
+		char ch='A';
+		for (int i = 1; i <=5; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+				
+				
+			}
+	*/		
+	//	}
+		
+	/*public static void main(String[] args) {
+		String s="LOTUS";
+		char [] arr=s.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j <=i; j++) {
+				System.out.print(arr[j]);
+				
+			}
+			System.out.println();
+		}
+			
+		}
+		
+	}*/
+	/*public static void main(String[] args) {
+		String s="APPLE";
+		int length=s.length();
+		char arr[]=s.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j <=i; j++) 
+				System.out.print(arr[j]);
+				System.out.println();
+				
+			}
+	
+		
+			for (int i =length; i >=0;i--) {
+				for (int j = 0; j <i; j++) 
+					System.out.print(arr[j]);
+				System.out.println();
+				
+				
+			
+}}}
+	*/
+}
