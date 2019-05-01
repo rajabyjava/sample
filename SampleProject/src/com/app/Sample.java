cbb333b167547c6c8b303c2856db9798e72af66d
rajabyjava <rajasekhar.pcr@gmail.com>
2019-05-01 10:38:37 +0530

    duplicate array in prints.. hjxzbcasbcgacb
    sajkcbdsahfhdjsfjsbf

--- SampleProject/src/com/app/Sample.java
+++ SampleProject/src/com/app/Sample.java
@@ -2,11 +2,23 @@ package com.app;
 
 public class Sample {
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
-		int c=a+b;
+		int temp;
+		int arr[]={1,1,2,3,3,4,4,5};
+		System.out.println();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j =i+1; j < arr.length; j++) {
+				if (arr[i]==arr[j]) 
+					/*temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+					*/
+				
+				
+			
+			System.out.println(arr[j]);
 		
-		System.out.println(c);
-	}
+		}}		
 
+
+}
 }
